8bd35af448a07a7c27c412397ffa3c234356eac8
Sergey <sergey@macbook.local>
2020-07-26 20:11:19 -0700

    Updates

---
+++ precache-manifest.32fab2a0c3850a84a428f64ba842ae47.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9efd810549bf77e1c57e3012e8aca19d",
+    "url": "/counter-redux/index.html"
+  },
+  {
+    "revision": "5c400dec27a68513490d",
+    "url": "/counter-redux/static/css/2.fb67120b.chunk.css"
+  },
+  {
+    "revision": "1ad34cd441c6fccf05ad",
+    "url": "/counter-redux/static/css/main.1b64581d.chunk.css"
+  },
+  {
+    "revision": "5c400dec27a68513490d",
+    "url": "/counter-redux/static/js/2.cc343ed4.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/counter-redux/static/js/2.cc343ed4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1ad34cd441c6fccf05ad",
+    "url": "/counter-redux/static/js/main.a437d20c.chunk.js"
+  },
+  {
+    "revision": "412fe60cbfc7ec94e966",
+    "url": "/counter-redux/static/js/runtime-main.abef93c9.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/counter-redux/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/counter-redux/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/counter-redux/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/counter-redux/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/counter-redux/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
